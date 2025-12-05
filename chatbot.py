3e1f2461b76eb3e747ff34e6913d7bab860f32d6
Kabilkirithik <kydykabil@gmail.com>
2025-12-05 11:05:35 +0530

    05/12/25

--- chatbot.py
+++ chatbot.py
@@ -8,34 +8,14 @@ llm = LLM(
 
 prompt = ""
 
-
-
-task_coardinator = Agent(role = "Task coardinator ",
-        goal = "Analyze user query and deligate to the appropriate tasks.",
-        backstory = "Expert is assigning task to the different agents",
-        llm = llm
-        )
-
- 
 chatbot = Agent(role = "Chatbot",
-        goal = f" Answer the question asked by the user ",
-        backstory = "Expert in answering the question",
+        goal = f" Answer and accomplish the task in '{prompt}' ",
+        backstory = "Expert in answering the question and assign the right task and complete it according the requirements",
         llm = llm
     )
-
-data_fetcher = ""
-
-
-
 
 chatbot_task = Task(
     description= "Answer to all the question asked by the user in prompt, and accomplish the task mentioned in the prompt by using the right tools for the right task",
     expected_output="Answer to the question with the tool output",
-    agent=chatbot
-)
 
-crew = Crew(
-    agents=[chatbot],
-    tasks=[chatbot_task],
-    verbose = True
 )
